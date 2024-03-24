f7bfb7fe051d3191744c1fc197b935754850fed2
chidubem100 <dubemernest23@gmail.com>
2024-03-24 15:58:25 +0100

    main-commit

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,29 @@
 #!/usr/bin/env node
 
+// console.log("hello fam")
 const { Command} = require("commander");
 const program = new Command();
-
+const {
+    addCommand,
+    listCommand,
+    removeCommand,
+    updateCommand,
+    searchCommand,
+    filterCommand
+} = require("../lib/commands");
+const packageJxon = require('../package.json');
 
 program
-    .version('1.0.0')
+    .version(packageJxon.version)
     .description("A command-line snippet manager")
     .showHelpAfterError("add --help for additional information")
+
+
+addCommand(program)
+listCommand(program)    
+removeCommand(program)
+updateCommand(program)
+searchCommand(program)
+filterCommand(program)
+
+program.parse(process.argv);
